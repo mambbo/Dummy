445ad3277a87dd95e9a36461b76e1c781a8b30a0
mambbo <mambbo@naver.com>
2017-02-06 14:50:46 +0900

    Update HelloGitHub.java

--- HelloGitHub/src/com/sist/HelloGitHub.java
+++ HelloGitHub/src/com/sist/HelloGitHub.java
@@ -6,12 +6,11 @@ public class HelloGitHub {
 		System.out.println("what's Git?");
 		System.out.println("It's DVCS");
 		System.out.println("It's collaboration");
-
 		System.out.println("Test");
 		System.out.println("fetched");
-
 		System.out.println("Conflict");
 		System.out.println("pushed");
+		System.out.println("Git pull testing");
 	
 
 
